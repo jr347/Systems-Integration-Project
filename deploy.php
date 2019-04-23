a53363556f12eed2667adce701ae326bedca7411
jr347 <jr347@njit.edu>
2019-04-22 21:53:27 -0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,6 +1,12 @@
 #!/usr/bin/php
 <?php
 
+//**************************************************************************************************************
+// Description:     This deploy.php file contains the file path and the target machine to scp the compressed
+//                  files to. This static data is used by a separate executable file that is triggered based 
+//                  on values passed. 
+//**************************************************************************************************************
+
 $filename = "/tmp/text_folder.tar.gz";
 $target = "kamran@192.168.43.125:/home/kamran/Desktop";
 echo "$filename $target";
